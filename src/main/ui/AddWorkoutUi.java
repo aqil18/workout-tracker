883cc14716f3d233346241e3cea30c8ab5dc00b8
aqilf <afaizala@students.cs.ubc.ca>
2023-03-24 00:52:52 -0700

    Made a visible home screen.

---
+++ src/main/ui/AddWorkoutUi.java
@@ -0,0 +1,12 @@
+package ui;
+
+import javax.swing.*;
+
+public class AddWorkoutUi {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton addButton;
+
+
+
+}
--- src/main/ui/WorkoutTrackerUi.java
+++ src/main/ui/WorkoutTrackerUi.java
@@ -1,13 +1,51 @@
 package ui;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class WorkoutTrackerUi extends JDialog {
-    private JPanel HomePanel;
+    private JPanel homePanel;
     private JButton addButton;
     private JButton viewButton;
     private JButton loadWorkoutsButton;
     private JButton saveButton;
 
 
+
+    public WorkoutTrackerUi() {
+        setContentPane(homePanel);
+        setSize(500, 500);
+
+        addButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        viewButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        loadWorkoutsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        saveButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+    }
+
+    public static void main(String[] args) {
+        WorkoutTrackerUi workoutTrackerUi = new WorkoutTrackerUi();
+        workoutTrackerUi.setVisible(true);
+    }
 }
